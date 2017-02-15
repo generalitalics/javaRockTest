cf21420c6401737c43bb46b91a69ce5e3a3c7e8f
generalitalics <generalitalics@mail.ru>
2017-02-15 11:29:30 +0300

    Созаю аналог web-теста на Selenide

--- mantis-tests/src/test/java/ru/ruru/st/mantis/tests/TestWithSelena.java
+++ mantis-tests/src/test/java/ru/ruru/st/mantis/tests/TestWithSelena.java
@@ -1,5 +1,6 @@
 package ru.ruru.st.mantis.tests;
 
+import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,6 +8,8 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
 
 import java.util.concurrent.TimeUnit;
 
@@ -14,9 +17,9 @@ import java.util.concurrent.TimeUnit;
 /**
  * Created by m.shoshin on 09.02.2017.
  */
-public class TestWithSelena extends TestBase{
+public class TestWithSelena {
 
-
+/*
         ChromeDriver wd;
 
         @BeforeMethod
@@ -24,10 +27,20 @@ public class TestWithSelena extends TestBase{
             wd = new ChromeDriver();
             wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
         }
-
+*/
         @Test
         public void testWithSelena() {
-            wd.get("http://localhost/addressbook/");
+
+            open("http://localhost/addressbook/");
+            $(By.name("user")).click();
+            $(By.name("user")).clear();
+            $(By.name("user")).val("admin");
+            $(By.name("pass")).click();
+            $(By.name("pass")).clear();
+            $(By.name("pass")).val("secret").pressEnter();
+
+
+            /*
             wd.findElement(By.name("user")).click();
             wd.findElement(By.name("user")).clear();
             wd.findElement(By.name("user")).sendKeys("admin");
@@ -43,13 +56,14 @@ public class TestWithSelena extends TestBase{
             wd.findElement(By.name("lastname")).clear();
             wd.findElement(By.name("lastname")).sendKeys("sdgsg");
             wd.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
+            */
         }
-
+/*
         @AfterMethod
         public void tearDown() {
             wd.quit();
         }
-
+*/
     public static boolean isAlertPresent(FirefoxDriver wd) {
         try {
             wd.switchTo().alert();
